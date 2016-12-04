1650a1a9a645085b5fe7ff493238e0915c09464f
iik1997 <igor.i.katkov@gmail.com>
2016-12-04 13:40:18 +0100

    1st commit

---
+++ dq/rus.php
@@ -0,0 +1,76 @@
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+<?php
+    $gt = "GabrielTech";
+    $worldVision = "<a href='http://www.worldvision.de' target='_blank'>World Vision</a>";
+    $welcome = "Добро пожаловать ";
+    $pageTitle = "$gt - Mы помогаем!";
+    $titleLine1 = "Почему GabrielTech?";
+    $titleLine2 = "Потому что мы думаем, что мы можем помочь небогатым людям сделать свою жизнь достойнее.";
+    $titleLine3 = "... и мы будем рады, если Вы присоединитесь к этому проекту!";
+    $titleLine4 = "Как?";
+    $titleLine5 = "Воспользуйтесь нашими сервисами";
+    $menuHome = "В начало";
+    $menuAbout = "О нас";
+    $menuService = "Сервис";
+    $menuContact = "Контакт";
+
+    $itemAppointment = "Назначить время"; 
+    $itemSupport = "Поддержка"; 
+    $itemRepair = "Починка"; 
+    $itemCharity = "Благотворительность"; 
+
+    $itemAppointmentText = "Выбрать время здесь, ниже";
+    $itemSupportText = "Встретимся, чтобы понять Вашу проблему"; 
+    $itemRepairText = "Мы починим Ваш компьютер и доставим Вам его обратно";
+    $itemCharityText = "Мы верим в честность людей. Вы жертвуете столько, сколько хотите, чтобы помочь нам помочь другим. Половину мы отдем напрямую в $worldVision (поддержка нуждающихся по всему миру) и на другие социальные проекты";
+
+    $serviceTitle = "Услуги, которые мы предлагаем";
+    $services1 = "Ремонт компьютеров";
+    $services1Item1 = "Microsoft Windows";
+    $services1Item2 = "Linux";
+    $services1Item3 = "Apple MacOSx";
+
+    $services2 = "Web-решения";
+    $services2Item1 = "Web-сайты";
+    $services2Item2 = "Системы электронной коммерции";
+    $services2Item3 = "Web-инструменты";
+
+    $services3 = "Дизайн пользовательского интерфейса";
+    $services3Item1 = "Визитки";
+    $services3Item2 = "Логотипы";
+    $services3Item3 = "Редактировние фото";
+
+    $services4 = "Разрботка ПО";
+    $services4Item1 = "Тренировки по Scrum-разработке";
+    $services4Item2 = "JIRA, Stash, Jenkins, Confluence";
+
+    $contactText = "Свяжитесь с нами";
+    $phoneText = "Телефон";
+    $phoneTextLabel = "Номер телефона";
+
+    $sendMessageText = "Послать сообщение";
+    
+    $formName = "Имя";
+    $formEmail = "E-mail";
+    $formMessage = "Сообщение";
+
+    $formNameText = "Имя & Фамилия";
+    $formEmailText = "example@domain.com";
+    $formYourAnswerText = "Ваш ответ";
+
+    $formErrorName = "Пожалуйста, введите Ваше имя";
+    $formErrorEmail = "Пожалуйста, введите актуальный e-mail";
+    $formErrorMessage = "Пожалуйста, введите сообщение";
+    $formErrorAntiSPAM = "Ваш anti-spam неверен";
+
+
+    $submitButtonText = "Отправить";
+
+    $designedbyText = "Дизайн проекта";
+
+    $openingHours = "Режим работы";
+    $openingHoursItem1 = "Понедельник - Пятница: 18:00 - 23:00";
+    $openingHoursItem2 = "Суббота - Воскресение: 08:00 - 20:00"; 
+
+
+?>
